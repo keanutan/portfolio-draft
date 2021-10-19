cdf254b66887fa861aa97d7c05527a66cb98fa9f
keanu-natchev <keanu.natchev@mail.mcgill.ca>
2021-10-19 06:25:04 -0400

    deployed newest version

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,22 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import './Footer.css';
 // import french from './french.svg'; // with import
-import french from './FooterImages/french.svg'; // with import
+// import french from './FooterImages/french.svg'; // with import
 import github from './FooterImages/github.svg'; // with import
 import linkedin from './FooterImages/linkedin.svg'; // with import
 // import linkedin from './linkedin.svg'; // with import
 
+// function usePersistedState(key, defaultValue) {
+//     const [state, setState] = React.useState(
+//         () => JSON.parse(localStorage.getItem(key)) || defaultValue
+//     );
+//     useEffect(() => {
+//         localStorage.setItem(key, JSON.stringify(state));
+//     }, [key, state]);
+//     return [state, setState];
+// }
+
 
 export default class Footer extends Component {
+    
+    state = {
+        language: 'Français',
+        
+    };
+    
+    languageSelect = () => {
+        // const [theme, setTheme] = usePersistedState('theme', 'light');
+        // usePersistedState('language', 'English'); 
+        (this.state.language == 'English') ? (this.setState({ language: 'Français' })) : (this.setState({ language: 'English' }));
+    }
+
     render() {
         return (
             <div className="page-footer">
-                <div className="empty-div"></div>
-                <footer className="copyright-footer"> <small> Copyright © 2021 Keanu Natchev</small> </footer>
                 <div className="social-media-links-languages">
-                    <img className="social-media-languages-footer-image" src={french} />
-                    <img className="social-media-languages-footer-image" src={github} />
-                    <img className="social-media-languages-footer-image" src={linkedin} />
+                    <a href="https://github.com/keanutan" target="_blank">
+                        <img className="social-media-links-languages-images" src={github} />
+                    </a>
+                    <a href="https://www.linkedin.com/in/keanu-natchev/" target="_blank">
+                        <img className="social-media-links-languages-images" src={linkedin} />
+                    </a>
+                </div>
+                <footer className="copyright-footer"> <small> Copyright © 2021 Keanu Natchev</small> </footer>
+                <div className="social-media-links-languages" onClick={this.languageSelect}>
+                    {this.state.language}
                 </div>
             </div>
         )
--- src/TechnicalSkills.js
+++ src/TechnicalSkills.js
@@ -5,6 +5,7 @@ export const TechnicalSkills = [
             'Java',
             'C',
             'Python',
+            'CUDA',
             'Javascript',
             'VHDL',
             'ARM Assembly',
