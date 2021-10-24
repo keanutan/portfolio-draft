b06359d6701e46a7e0cf2ce13b5455aa6da8d1e7
keanu-natchev <keanu.natchev@mail.mcgill.ca>
2021-10-24 08:19:11 -0400

    Technical Skills chnaged from strings to images.

--- src/TechnicalSkills.js
+++ src/TechnicalSkills.js
@@ -2,53 +2,60 @@ export const TechnicalSkills = [
     {
         skillTitle: 'Programming Languages',
         skillElements: [
-            'Java',
-            'C',
-            'Python',
-            'CUDA',
-            'Javascript',
-            'VHDL',
-            'ARM Assembly',
-            'BASH'
+            <img alt="Java" src="https://shields.io/badge/JAVA-333333?logo=java&style=for-the-badge&logoColor=white"></img>,
+            <img alt="C" src="https://shields.io/badge/c-333333?logo=c&style=for-the-badge&logoColor=white"></img>,
+            <img alt="Python" src="https://shields.io/badge/Python-333333?logo=python&style=for-the-badge&logoColor=white"></img>,
+            <img alt="CUDA" src="https://shields.io/badge/CUDA-333333?logo=nvidia&style=for-the-badge&logoColor=white"></img>,
+            <img alt="HTML" src="https://shields.io/badge/HTML5-333333?logo=html5&style=for-the-badge&logoColor=white"></img>,
+            <img alt="Javascript" src="https://shields.io/badge/Javascript-333333?logo=javascript&style=for-the-badge&logoColor=white"></img>,
+            <img alt="CSS" src="https://shields.io/badge/CSS3-333333?logo=css3&style=for-the-badge&logoColor=white"></img>,
+            <img alt="VHDL" src="https://shields.io/badge/VHDL-333333?&style=for-the-badge&logoColor=white"></img>,
+            <img alt="Arm Assembly" src="https://shields.io/badge/ARM-333333?&style=for-the-badge&logoColor=white"></img>,
+            <img alt="BASH" src="https://shields.io/badge/BASH-333333?logo=gnubash&style=for-the-badge&logoColor=white"></img>,
         ]
     },
     {
         skillTitle: 'Tools',
         skillElements: [
-            'Git',
-            'Cucumber.js',
-            'Linux',
-            'Gradle',
-            'Spring Boot',
-            'Heroku',
-            'Maven',
-            'Travis CI'
+            <img alt="Git" src="https://shields.io/badge/Git-333333?logo=git&style=for-the-badge&logoColor=white"></img>,
+            <img alt="Cucumber.js" src="https://shields.io/badge/cucumber.js-333333?logo=cucumber&style=for-the-badge&logoColor=white"></img>,
+            <img alt="Gradle" src="https://shields.io/badge/Gradle-333333?logo=gradle&style=for-the-badge&logoColor=white"></img>,
+            <img alt="Maven" src="https://shields.io/badge/Maven-333333?logo=apachemaven&style=for-the-badge&logoColor=white"></img>,
+            <img alt="Heroku" src="https://shields.io/badge/Heroku-333333?logo=heroku&style=for-the-badge&logoColor=white"></img>,
+            <img alt="Travis CI" src="https://shields.io/badge/Tarvis%20CI-333333?logo=travisci&style=for-the-badge&logoColor=white"></img>
         ]
     },
     {
         skillTitle: 'Frameworks',
         skillElements: [
-            'React.js',
-            'Vue.js'
+            <img alt="Spring Boot" src="https://shields.io/badge/Spring%20Boot-333333?logo=springboot&style=for-the-badge&logoColor=white"></img>,
+            <img alt="React" src="https://shields.io/badge/React-333333?logo=react&style=for-the-badge&logoColor=white"></img>,
+            <img alt="Vue" src="https://shields.io/badge/Vue.js-333333?logo=vuedotjs&style=for-the-badge&logoColor=white"></img>
         ]
     },
     {
         skillTitle: 'Development Environments',
         skillElements: [
-            'Visual Studio Code',
-            'Eclipse',
-            'Adobe Dreamweaver'
+            <img alt="Visual Studio Code" src="https://shields.io/badge/Visual%20Studio%20Code-333333?logo=visualstudiocode&style=for-the-badge&logoColor=white"></img>,
+            <img alt="Eclipse" src="https://shields.io/badge/Eclipse-333333?logo=eclipseide&style=for-the-badge&logoColor=white"></img>,
+            <img alt="Adobe Dreamweaver" src="https://shields.io/badge/Adobe%20Dreamweaver-333333?logo=adobedreamweaver&style=for-the-badge&logoColor=white"></img>
+        ]
+    },
+    {
+        skillTitle: 'Operating Systems',
+        skillElements: [
+            <img alt="Windows" src="https://shields.io/badge/Windows-333333?logo=windows&style=for-the-badge&logoColor=white"></img>,
+            <img alt="Linux" src="https://shields.io/badge/Linux-333333?logo=linux&style=for-the-badge&logoColor=white"></img>,
+            <img alt="Ubuntu" src="https://shields.io/badge/Ubuntu-333333?logo=ubuntu&style=for-the-badge&logoColor=white"></img>
         ]
     },
     {
         skillTitle: 'Other',
         skillElements: [
-            'Figma',
-            'Adobe Photoshop',
-            'Adobe Premiere Pro',
-            'MS Office Suite',
-            'Webots',
-            'LeoCAD'
+            <img alt="Figma" src="https://shields.io/badge/Figma-333333?logo=figma&style=for-the-badge&logoColor=white"></img>,
+            <img alt="Adobe Photoshop" src="https://shields.io/badge/Adobe%20Photoshop-333333?logo=adobephotoshop&style=for-the-badge&logoColor=white"></img>,
+            <img alt="Adobe Premiere Pro" src="https://shields.io/badge/Adobe%20Premiere%20Pro-333333?logo=adobepremierepro&style=for-the-badge&logoColor=white"></img>,
+            <img alt="Microsoft Office" src="https://shields.io/badge/Microsoft%20Office-333333?logo=microsoftoffice&style=for-the-badge&logoColor=white"></img>
         ]
     },
 ]
