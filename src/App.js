41468cce4604252f9b6a2ef3d61752addfdc04f1
Keanu <keanu@Mr-Andersons-MacBook-Pro.local>
2025-03-16 16:22:11 -0400

    Deploy test.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 // import { BrowserRouter as Router, Link, Switch } from 'react-router-dom';
 // import Switch from 'react-router-dom/Switch';
 import { Switch } from 'react-router-dom';
